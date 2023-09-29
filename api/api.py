039e2e20fffcffaaddabe2ddc499235641b077fe
tebit <tibet.tb@gmail.com>
2023-09-29 14:39:03 +0700

    Added clock and WIP weather function

--- api/api.py
+++ api/api.py
@@ -1,8 +1,31 @@
-from flask import Flask
-import time
+from flask import Flask, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+import requests
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-@app.route('/main')
-def get_current_time():
-    return {'time': time.time()}
+# Configure CORS for the '/main' and '/weather' endpoints
+cors = CORS(app, resources={r"/weather": {"origins": "*"}})
+
+@app.route('/weather')
+def get_weather():
+    load_dotenv()
+    api_key = os.getenv('WEATHER_KEY')  # Replace with your API key
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+
+    if not lat or not lng:
+        return {'error': 'Latitude and longitude are required parameters.'}
+
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&appid={api_key}'
+    weather_response = requests.get(weather_url)
+    weather_data = weather_response.json()
+
+    if weather_data.get('status') == 'OK':
+        weather_info = weather_data['results'][0]['name']
+        print(weather_info)
+        return {'location': f'Lat: {lat}, Lng: {lng}', 'weather': weather_info}
+    else:
+        return {'error': 'Could not fetch weather data.'}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [currentLocation, setCurrentLocation] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        fetch(`/weather?lat=${latitude}&lng=${longitude}`)
+          .then(res => res.json())
+          .then(data => {
+            setCurrentLocation(data.location);
+          });
+      });
+    }
+
+    return () => clearInterval(interval); // Clean up the interval on component unmount
+  }, []);
+
+  const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+  const formattedTime = currentTime.toLocaleTimeString('en-US', options);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +43,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>Current time is {formattedTime}.</p>
+        <p>Your current location is {currentLocation}.</p>
       </header>
     </div>
   );
