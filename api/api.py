6247be0bb88800d3199d17f53b91a1baaeaedb66
tebit <tibet.tb@gmail.com>
2023-09-30 16:05:52 +0700

    Finish-up the weather functions and updated the UI

--- api/api.py
+++ api/api.py
@@ -7,10 +7,29 @@ import os
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 # Configure CORS for the '/main' and '/weather' endpoints
-cors = CORS(app, resources={r"/weather": {"origins": "*"}})
+cors = CORS(app, resources={r"/rain": {"origins": "*"}, r"/temp": {"origins": "*"}})
 
-@app.route('/weather')
-def get_weather():
+@app.route('/rain')
+def get_rainfall():
+    load_dotenv()
+    api_key = os.getenv('WEATHERBIT_KEY')  # Replace with your API key
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+
+    if not lat or not lng:
+        return {'error': 'Latitude and longitude are required parameters.'}
+
+    weather_url = f'https://api.weatherbit.io/v2.0/forecast/daily?lat={lat}&lon={lng}&key={api_key}'
+    weather_response = requests.get(weather_url)
+    weather_data = weather_response.json()
+
+    if weather_data:
+        return {'percip': weather_data['data'][0]['pop']}
+    else:
+        return {'error': 'Could not fetch weather data.'}
+    
+@app.route('/temp', methods=['GET'])
+def get_temp():
     load_dotenv()
     api_key = os.getenv('WEATHER_KEY')  # Replace with your API key
     lat = request.args.get('lat')
@@ -19,13 +38,10 @@ def get_weather():
     if not lat or not lng:
         return {'error': 'Latitude and longitude are required parameters.'}
 
-    weather_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&appid={api_key}'
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lng}&appid={api_key}&units=metric'
     weather_response = requests.get(weather_url)
     weather_data = weather_response.json()
-
-    if weather_data.get('status') == 'OK':
-        weather_info = weather_data['results'][0]['name']
-        print(weather_info)
-        return {'location': f'Lat: {lat}, Lng: {lng}', 'weather': weather_info}
+    if weather_data:
+        return {'temp': weather_data['main']['temp'], 'feels': weather_data['main']['feels_like']}
     else:
         return {'error': 'Could not fetch weather data.'}
--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
-  const [currentLocation, setCurrentLocation] = useState('');
+  const [currentTemp, setTemp] = useState(0);
+  const [currentFeels, setFeels] = useState(0);
+  const [currentRain, setRain] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
-        fetch(`/weather?lat=${latitude}&lng=${longitude}`)
+        fetch(`/temp?lat=${latitude}&lng=${longitude}`)
           .then(res => res.json())
           .then(data => {
-            setCurrentLocation(data.location);
+            setTemp(data.temp);
+            setFeels(data.feels);
           });
-      });
-    }
-
-    return () => clearInterval(interval); // Clean up the interval on component unmount
+        fetch(`/rain?lat=${latitude}&lng=${longitude}`)
+          .then(res => res.json())
+          .then(data => {
+            setRain(data.percip);
+          });
+        });
+      }
+    }, 5000); // need to change to 1 hour in the real app 
+    return () => clearInterval(interval);
   }, []);
 
   const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
@@ -31,20 +42,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Current time is {formattedTime}.</p>
-        <p>Your current location is {currentLocation}.</p>
+        <div className="app-container">
+          <div className="block left-block">
+            <div className="info">
+              <p>Current time is {formattedTime}.</p>
+              <p>Current temp is {currentTemp} and feels like {currentFeels}.</p>
+              <p>Percipitation percentage is {currentRain}%.</p>
+            </div>
+            <div className="icons">
+              {/* Add icons here */}
+            </div>
+          </div>
+          <div className="block right-block">
+            <div className="section bus">
+              <h2>Bus</h2>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+            </div>
+            <div className="section destination">
+              <h2>Destination</h2>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+            </div>
+            <div className="section arrival">
+              <h2>Arrival</h2>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+              <div className="box"></div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
