f4ecb150e430200510ca7aab77444f7d60eed6b2
tebit <tibet.tb@gmail.com>
2023-10-02 07:49:23 +0700

    First prototype of UI done. Finished most functions.

--- api/api.py
+++ api/api.py
@@ -3,11 +3,14 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import requests
 import os
+import csv
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 # Configure CORS for the '/main' and '/weather' endpoints
-cors = CORS(app, resources={r"/rain": {"origins": "*"}, r"/temp": {"origins": "*"}})
+cors = CORS(app, resources={r"/rain": {"origins": "*"}, r"/temp": {"origins": "*"}, r"/bus": {"origins": "*"}})
 
 @app.route('/rain')
 def get_rainfall():
@@ -19,15 +22,31 @@ def get_rainfall():
     if not lat or not lng:
         return {'error': 'Latitude and longitude are required parameters.'}
 
-    weather_url = f'https://api.weatherbit.io/v2.0/forecast/daily?lat={lat}&lon={lng}&key={api_key}'
-    weather_response = requests.get(weather_url)
-    weather_data = weather_response.json()
 
-    if weather_data:
-        return {'percip': weather_data['data'][0]['pop']}
-    else:
-        return {'error': 'Could not fetch weather data.'}
+
+    # weather_url = f'https://api.weatherbit.io/v2.0/forecast/daily?lat={lat}&lon={lng}&key={api_key}'
+    # weather_response = requests.get(weather_url)
+    # weather_data = weather_response.json()
     
+    # percip = weather_data['data'][0]['pop']
+    # location = weather_data['city_name']
+    percip = 50
+    location = 'Pathum Wan'
+    if percip < 50:
+        msg = 'Rain warning'
+    elif percip >= 50 and percip < 75:
+        msg = 'Moderate rain warning'
+    elif percip >= 75:
+        msg = 'Heavy rain warning'
+    else:
+        msg = 'Clear skies' 
+
+    return {'percip': percip, 'location' : location, 'msg': msg}
+    # if weather_data:
+    #     return {'percip': percip, 'location' : location, 'msg': msg}
+    # else:
+        # return {'error': 'Could 5 fetch weather data.'}
+
 @app.route('/temp', methods=['GET'])
 def get_temp():
     load_dotenv()
@@ -42,6 +61,26 @@ def get_temp():
     weather_response = requests.get(weather_url)
     weather_data = weather_response.json()
     if weather_data:
-        return {'temp': weather_data['main']['temp'], 'feels': weather_data['main']['feels_like']}
+        print(type(weather_data['main']['temp']))
+        return {'temp': round(weather_data['main']['temp']), 'feels': round(weather_data['main']['feels_like'])}
     else:
         return {'error': 'Could not fetch weather data.'}
+    
+@app.route('/bus')
+def bus_info():
+    df = pd.read_csv('busss - Sheet1.csv', header=None)
+    df['Bus_No'] = df[0].str.split(' ').str[0]
+
+    sample = df.sample(1)
+    data = pd.DataFrame(columns=['Bus_No', 'Destination', 'ETA'])
+    data['Bus_No'] = sample['Bus_No']
+    data['Destination'] = sample[2].str.strip()
+    # random time from 1 to sample[4]
+    data['ETA'] = np.random.randint(1, sample[4])
+    #assign new row index
+    data.index = range(len(data))
+    # turn into  dictionary
+    output = dict()
+    for i, d in enumerate(data.to_dict('records')):
+        output[i] = d
+    return output[0]
--- src/App.js
+++ src/App.js
@@ -1,42 +1,141 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+function getFormattedDate() {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const now = new Date();
+
+  const day = days[now.getDay()];
+  const month = months[now.getMonth()];
+  const date = now.getDate();
+
+  return `${day}, ${month} ${date}`;
+}
+
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [currentTemp, setTemp] = useState(0);
-  const [currentFeels, setFeels] = useState(0);
   const [currentRain, setRain] = useState(0);
+  const [currentLocale, setLocale] = useState('Bangkok');
+  const [currentMsg, setMsg] = useState('No rain warning');
+  const [busData, setBusData] = useState([]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
+  
+ 
+  const fetchData = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
+        
         fetch(`/temp?lat=${latitude}&lng=${longitude}`)
           .then(res => res.json())
           .then(data => {
             setTemp(data.temp);
-            setFeels(data.feels);
           });
+        
         fetch(`/rain?lat=${latitude}&lng=${longitude}`)
           .then(res => res.json())
           .then(data => {
             setRain(data.percip);
+            setLocale(data.location);
+            setMsg(data.msg);
           });
-        });
-      }
-    }, 5000); // need to change to 1 hour in the real app 
+      });
+    }
+  };
+
+  const fetchBusData = () => {
+    fetch("/bus")
+      .then((res) => res.json())
+      .then((data) => {
+        const existingBus = busData.find((bus) => bus.Bus_No === data.Bus_No);
+        if (!existingBus) {
+          setBusData((prevData) => [...prevData, { ...data, id: Date.now() }]); // Assign a unique id
+        } else {
+          if (data.ETA < existingBus.ETA) {
+            setBusData((prevData) =>
+              prevData.map((bus) =>
+                bus.Bus_No === data.Bus_No ? { ...bus, ETA: data.ETA } : bus
+              )
+            );
+          }
+        }
+      });
+  };  
+  
+  useEffect(() => {
+    fetchBusData();  
+    const interval = setInterval(() => {
+      fetchBusData();
+    }, 60000);
+  
     return () => clearInterval(interval);
   }, []);
 
-  const options = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+  useEffect(() => {
+    const updateBusData = () => {
+      setCurrentTime(new Date());
+      setBusData((prevData) =>
+        prevData.map((bus) => {
+          if (bus.ETA !== "Soon") {
+            return { ...bus, ETA: bus.ETA > 1 ? bus.ETA - 1 : "Soon" };
+          }
+          return bus;
+        })
+      );
+  
+      // Remove buses with ETA "Soon" after 1 minute
+      setBusData((prevData) =>
+        prevData.filter((bus) => {
+          if (bus.ETA === "Soon") {
+            return bus.timestamp > Date.now() - 60000; // Keep buses added within the last minute
+          }
+          return true;
+        })
+      );
+    };
+
+    // Update time and handle ETA
+    const interval = setInterval(updateBusData, 60000);
+  
+    return () => {
+      clearInterval(interval);
+      };
+    }, [busData]);
+  
+
+  useEffect(() => {
+    fetchData(); // Fetch data once before starting the interval
+
+    const interval = setInterval(() => {
+      fetchData(); // Fetch data at intervals
+    }, 3600000); // Change to 1 hour in the real app
+
+    return () => clearInterval(interval);
+  }, []);
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setCurrentTime(new Date());
+    setBusData((prevData) =>
+      prevData.map((bus) => ({
+        ...bus,
+        ETA: bus.ETA > 0 ? bus.ETA - 1 : bus.ETA === "Soon" ? "Soon" : 0,
+      }))
+    );
+
+    // Remove buses with ETA 0 or "Soon"
+    setBusData((prevData) => prevData.filter((bus) => bus.ETA !== 0 && bus.ETA !== "Soon"));
+  }, 60000); // Update time and remove completed buses every minute
+
+  return () => clearInterval(interval);
+}, []);
+
+
+  // ... rest of the code remains the same
+
+  const options = { hour: '2-digit', minute: '2-digit', hour12: false };
   const formattedTime = currentTime.toLocaleTimeString('en-US', options);
 
   return (
@@ -44,45 +143,51 @@ function App() {
       <header className="App-header">
         <div className="app-container">
           <div className="block left-block">
-            <div className="info">
-              <p>Current time is {formattedTime}.</p>
-              <p>Current temp is {currentTemp} and feels like {currentFeels}.</p>
-              <p>Percipitation percentage is {currentRain}%.</p>
-            </div>
             <div className="icons">
               {/* Add icons here */}
             </div>
+            <div className="box-container">
+              <div className="box top">
+                <p className='box-text-time'>{formattedTime}</p>
+                <p className='box-text-date'>{getFormattedDate()}</p>
+
+              </div>
+              <div className="box"></div>
+              <div className="box bottom">
+                <p className='box-text'>{currentLocale}</p>  
+                <p className='box-text-temp'>{currentTemp}°</p>  
+                <p className='box-text-msg'>{currentMsg}</p>
+              </div>
+            </div>
           </div>
           <div className="block right-block">
-            <div className="section bus">
-              <h2>Bus</h2>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-            </div>
-            <div className="section destination">
-              <h2>Destination</h2>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-            </div>
-            <div className="section arrival">
-              <h2>Arrival</h2>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
-              <div className="box"></div>
+          <div className="section bus">
+          <div className="bus-table-container">
+                <table className="bus-table">
+                  <thead>
+                    <tr className="header-wrapper">
+                      <th>Route</th>
+                      <th>Destination</th>
+                      <th>Arrival</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {busData.slice(0, 5).map((bus) => (
+                        <tr key={`${bus.Bus_No}-${bus.Destination}`} className='child-wrapper'>
+                          <td>{bus.Bus_No}</td>
+                          <td>{bus.Destination}</td>
+                          <td>{bus.ETA === 0 ? "Soon" : bus.ETA === "Soon" ? bus.ETA : bus.ETA + " min"}</td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
       </header>
     </div>
   );
-}
+} 
 
 export default App;
